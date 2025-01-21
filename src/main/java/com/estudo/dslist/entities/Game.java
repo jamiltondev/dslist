05573b1558cc8e015c85dd572d3be2ffd35105f0
Jamilton <jamilton.dev@gmail.com>
2025-01-20 21:40:47 -0300

    Criado entidade Game

---
+++ src/main/java/com/estudo/dslist/entities/Game.java
@@ -0,0 +1,68 @@
+package com.estudo.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String titulo;
+
+    @Column(name = "game_year")
+    private Integer ano;
+
+    @Column(name = "genre")
+    private String genero;
+
+    @Column(name = "platforms")
+    private String plataformas;
+
+    @Column(name = "score")
+    private String pontuacao;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "short_description", columnDefinition = "TEXT")
+    private String descricaoCurta;
+
+    @Column(name = "long_description", columnDefinition = "TEXT")
+    private String descricaoLonga;
+
+    public Game() {
+    }
+
+    public Game(Long id, String titulo, Integer ano, String genero, String plataformas, String pontuacao, String imgUrl, String descricaoCurta, String descricaoLonga) {
+        this.id = id;
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.plataformas = plataformas;
+        this.pontuacao = pontuacao;
+        this.imgUrl = imgUrl;
+        this.descricaoCurta = descricaoCurta;
+        this.descricaoLonga = descricaoLonga;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
